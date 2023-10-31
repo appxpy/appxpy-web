c953d25b54032b17f77d43bf7a27e1b324a44eec
appxpy <appxpy@appxpy.com>
2023-10-31 11:47:09 +0300

    fix!

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
 import svgr from '@svgr/rollup';
+import react from '@vitejs/plugin-react-swc';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,5 +10,17 @@ export default defineConfig({
   ],
   server: {
     host: '0.0.0.0'
-  }
+  },
+  build: {
+    rollupOptions: {
+      maxParallelFileOps: 1,
+      cache: false,
+      output: {
+        sourcemap: false,
+        manualChunks: {
+          three: ['three']
+        }
+      },
+    }
+  },
 })
